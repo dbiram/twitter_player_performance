5616dae864bbcc3ab6d016259eda3a2935346513
dbiram <mohamed-moez.dbira@student.ecp.fr>
2022-09-25 09:51:34 +0200

    add function to fetch last 24h tweets only

--- twitter/TwitterPlayerSearch.py
+++ twitter/TwitterPlayerSearch.py
@@ -2,6 +2,7 @@ from config import Config
 import tweepy
 import requests
 import json
+import datetime
 
 
 class TwitterPlayerSearch:
@@ -14,18 +15,27 @@ class TwitterPlayerSearch:
                            wait_on_rate_limit=True)
 
     @staticmethod
-    def search(player_name, player_account, team_account, max_tweets=100):
+    def search(player_name, start_date = None, max_tweets=100):
         query = f"""{player_name} lang:en -is:retweet OR {player_name} lang:de -is:retweet"""
         try:
 
             tweets = TwitterPlayerSearch.client.search_recent_tweets(query=query,
                                                       tweet_fields=['author_id', 'created_at'],
-                                                      max_results=max_tweets)
+                                                      max_results=max_tweets,
+                                                      start_time=start_date)
             print("-- Successfully connected to Twitter API --")
             return tweets
         except Exception as e:
             print("-- Couldn't connect to Twitter API --")
             print(e)
+
+    @staticmethod
+    def search_last_day(player_name, max_tweets=100):
+        now = datetime.datetime.now()
+        start_date = now - datetime.timedelta(days=1)
+        s = datetime.datetime.strftime(start_date, "%Y-%m-%d %H:%M:%S")
+        print(f"-- Fetching Tweets From {s} --")
+        return TwitterPlayerSearch.search(player_name, start_date=start_date, max_tweets=max_tweets)
 
     @staticmethod
     def jprint(obj):
