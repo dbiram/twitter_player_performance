d76261ed78916a577a4262d2325d9a47ec2e45b5
dbiram <mohamed-moez.dbira@student.ecp.fr>
2022-10-05 21:00:54 +0200

    create AppFunctions class that has functions used to query FootballAPI and get data for players in data_sample

---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+from app.app_functions import AppFunctions
--- app/app_functions.py
+++ app/app_functions.py
@@ -1,17 +1,44 @@
 from bundesliga_api.BundesligaAPI import BundesligaAPI
-import pandas as pd
+from data import AppData
+from utils import Fixture, PlayerStat
 
-from utils import Fixture
 
+class AppFunctions:
+    path = str
+    data = AppData
 
-def get_relevant_matches(date):
-    teams = pd.read_csv("data/samples/teams_mapping.csv", sep = ";")
-    matches = BundesligaAPI.get_list_matches(date).json()["response"]
-    relevant = []
+    def __init__(self, path="../data/samples/"):
+        self.path = path
+        self.data = AppData(path)
 
-    for match in matches:
-        if (match["teams"]["home"]["id"] in teams.football_api_team_id.to_list()) or (
-                match["teams"]["away"]["id"] in teams.football_api_team_id.to_list()):
-            relevant.append(Fixture(match["fixture"]["id"], match["teams"]["home"]["id"], match["teams"]["away"]["id"]))
+    def get_relevant_matches(self, date):
+        teams = self.data.teams_mapping
+        matches = BundesligaAPI.get_list_matches(date).json()["response"]
+        relevant = []
 
-    return relevant
+        for match in matches:
+            if (match["teams"]["home"]["id"] in teams.football_api_team_id.to_list()) or (
+                    match["teams"]["away"]["id"] in teams.football_api_team_id.to_list()):
+                relevant.append(
+                    Fixture(match["fixture"]["id"], match["teams"]["home"]["id"], match["teams"]["away"]["id"], date))
+
+        return relevant
+
+    def get_relevant_player_stats(self, fixture):
+        stats = BundesligaAPI.player_stats_from_fixture_id(fixture.fixture_id).json()["response"]
+
+        relevant = []
+
+        for s in stats:
+            for player in s["players"]:
+                id_api = player["player"]["id"]
+                if id_api in self.data.players_mapping.football_api_player_id.to_list():
+                    id_local = self.data.players_mapping[
+                        self.data.players_mapping.football_api_player_id == id_api].player_id.values[0]
+                    player_name = self.data.players[self.data.players.player_id == id_local].player_name.values[0]
+                    rating = player["statistics"][0]["games"]["rating"]
+                    if rating:
+                        relevant.append(PlayerStat(id_local, player_name, float(rating), fixture.date))
+                    else:
+                        relevant.append(PlayerStat(id_local, player_name, 0, fixture.date))
+        return relevant
