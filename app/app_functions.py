e21cdbadcbca14ecee072d504e80276c317144f6
dbiram <mohamed-moez.dbira@student.ecp.fr>
2022-12-25 00:55:12 +0100

    Save master data in HDFS under an AVRO files

--- app/app_functions.py
+++ app/app_functions.py
@@ -7,15 +7,23 @@ from utils import Fixture, PlayerStat, PlayerTweet
 from datetime import datetime
 import pandas as pd
 import csv
+import hdfs
+from dataclasses import asdict
+import fastavro
 
 
 class AppFunctions:
     path = str
     data = AppData
+    hdfs_client = hdfs.Client
 
-    def __init__(self, path="../data/"):
+    def __init__(self, hdfs_path=None, path="../data/"):
         self.path = path
         self.data = AppData(path)
+        if hdfs_path:
+            self.hdfs_client = hdfs.InsecureClient(hdfs_path)
+        else:
+            self.hdfs_client = None
 
     def get_relevant_matches(self, date):
         teams = self.data.teams_mapping
@@ -60,23 +68,85 @@ class AppFunctions:
                     relevant.append(PlayerTweet(player_id, player, tweet["text"], creation_date, date))
         return relevant
 
+    def add_list_to_avro(self, type, list):
+        if type == "fixture":
+            schema = {
+                "type": "record",
+                "namespace": "twitter_player_performance",
+                "name": "Fixture",
+                "doc": "Relevant fixtures (contains tracked teams)",
+                "fields": [
+                    {"name": "id$", "type": "long"},
+                    {"name": "fixture_id", "type": "int"},
+                    {"name": "home_team", "type": "int"},
+                    {"name": "away_team", "type": "int"},
+                    {"name": "date", "type": {"type": "int", "logicalType": "time-millis"}}
+                ]
+            }
+        elif type == "player":
+            schema = {
+                "type": "record",
+                "namespace": "twitter_player_performance",
+                "name": "Player",
+                "doc": "Relevant players ratings during fixtures (contains tracked players)",
+                "fields": [
+                    {"name": "id$", "type": "long"},
+                    {"name": "player_id", "type": "int"},
+                    {"name": "player_name", "type": "string"},
+                    {"name": "rating", "type": "float"},
+                    {"name": "date", "type": {"type": "int", "logicalType": "time-millis"}}
+                ]
+            }
+        elif type == "tweet":
+            schema = {
+                "type": "record",
+                "namespace": "twitter_player_performance",
+                "name": "Tweet",
+                "doc": "Tweets fetched about relevant players (contains tracked players)",
+                "fields": [
+                    {"name": "id$", "type": "long"},
+                    {"name": "player_id", "type": "int"},
+                    {"name": "player_name", "type": "string"},
+                    {"name": "tweet_body", "type": "string"},
+                    {"name": "created_at", "type": {"type": "int", "logicalType": "time-millis"}},
+                    {"name": "date", "type": {"type": "int", "logicalType": "time-millis"}}
+                ]
+            }
+
+        def add_id(dict_arg):
+            dict_arg["id$"] = int(datetime.now().timestamp() * 1000)
+            dict_arg["date"] = int(datetime.timestamp(dict_arg["date"]))
+            if "created_at" in dict_arg:
+                dict_arg["created_at"] = int(datetime.timestamp(dict_arg["created_at"]))
+            return dict_arg
+
+        data = [add_id(asdict(item)) for item in list]
+        with self.hdfs_client.write("/data/master_data/all_" + type + "s.avro", append=True) as avro_file:
+            fastavro.writer(avro_file, schema, data)
+
     def append_master_data(self, date):
 
         matches = self.get_relevant_matches(date)
-        if len(matches)>0:
+        if self.hdfs_client:
+            self.add_list_to_avro("fixture", matches)
+        if len(matches) > 0:
             for fixture in matches:
 
                 with open(self.path+'master_data/all_fixtures.csv', 'a+', newline='', encoding="utf-8") as write_obj:
                     csv_writer = csv.writer(write_obj, delimiter=';')
                     csv_writer.writerow([int(datetime.now().timestamp() * 1000), fixture.fixture_id, fixture.home_team_id, fixture.away_team_id, fixture.date])
                 players = self.get_relevant_player_stats(fixture)
-                if len(players)>0:
+                if self.hdfs_client:
+                    self.add_list_to_avro("player", players)
+                if len(players) > 0:
                     for player in players:
 
                         with open(self.path + 'master_data/all_player_rates.csv', 'a+', newline='', encoding="utf-8") as write_obj:
                             csv_writer = csv.writer(write_obj, delimiter=';')
                             csv_writer.writerow([int(datetime.now().timestamp() * 1000), player.player_id, player.player_name, player.rating, player.date])
         tweets = self.get_relevant_tweets(date)
+        if self.hdfs_client:
+            self.add_list_to_avro("tweet", tweets)
         tweets_df = {"id$": [], "player_id": [], "player_name": [], "tweet_body": [], "created_at": [], "date": []}
         if len(tweets) > 0:
             for tweet in tweets:
--- handlers.py
+++ handlers.py
@@ -2,5 +2,5 @@ from app.app_functions import AppFunctions
 from datetime import datetime, timedelta
 
 if __name__ == "__main__":
-    app = AppFunctions("data/")
+    app = AppFunctions(hdfs_path="http://localhost:50070", path="data/")
     app.append_master_data(datetime.now()-timedelta(days=1))
