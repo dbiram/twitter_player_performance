b5bb32c81dd9f002ac0c1d6facf67c59982eee9c
dbiram <mohamed-moez.dbira@student.ecp.fr>
2023-04-16 12:25:22 +0200

    Add handlers arguments for extraction date

--- handlers.py
+++ handlers.py
@@ -1,6 +1,18 @@
 from app.app_functions import AppFunctions
 from datetime import datetime, timedelta
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) > 4:
+        raise ValueError('Too many arguments passed')
+    args_dict = {"days": 0, "hours": 0, "minutes": 0}
+    for i, arg in enumerate(sys.argv):
+        if i == 1:
+            args_dict["days"] = int(arg)
+        elif i == 2:
+            args_dict["hours"] = int(arg)
+        elif i == 3:
+            args_dict["minutes"] = int(arg)
     app = AppFunctions(hdfs_path="http://localhost:50070", path="data/")
-    app.append_master_data(datetime.now()-timedelta(days=1))
+    app.append_master_data(datetime.now()-timedelta(days=args_dict["days"], hours=args_dict["hours"], minutes=args_dict["minutes"]))
+
